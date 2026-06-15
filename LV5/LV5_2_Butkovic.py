367d0b30d9fce3b0162769234175ede77226bce7
BartolB <bartolbutkovic3@gmail.com>
2026-06-15 11:57:29 +0200

    Add files via upload

---
+++ LV5/LV5_2_Butkovic.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score, accuracy_score, classification_report
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+
+
+
+df = pd.read_csv('C://Users//student//Desktop//labos//occupancy_processed.csv')
+
+feature_names = ['S3_Temp', 'S5_CO2']
+target_name = 'Room_Occupancy_Count'
+class_names = ['Slobodna', 'Zauzeta']
+
+X = df[feature_names].to_numpy()
+y = df[target_name].to_numpy()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_s = scaler.fit_transform(X_train)
+X_test_s = scaler.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train_s, y_train)
+
+
+y_pred = knn.predict(X_test_s)
+
+cm = confusion_matrix(y_test, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Class 0', 'Class 1'])
+disp.plot(cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.show()
+
+# Izracunaj preciznost
+precision = precision_score(y_test, y_pred)
+# Izracunaj odziv
+recall = recall_score(y_test, y_pred)
+# Izracunaj tocnost
+accuracy = accuracy_score(y_test, y_pred)
+# Report
+print(classification_report(y_test, y_pred))
